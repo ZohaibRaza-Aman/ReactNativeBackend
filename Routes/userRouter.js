d041ff7d19f8f5b27eca33fd5663fec4e7873fb4
zohaibraza <zohaibraza510@gmail.com>
2023-07-15 21:44:21 +0500

    h

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const route = express.Router();
 const userController = require("../Controller/userController");
-const AuthController = require("../Controller/authController");
 
-route.get("/test", AuthController.Protected, (req, res) => {
-  res.send("/User Valid");
-});
+
 
 route.get("/", userController.getUsers);
 
